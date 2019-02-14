5cb773425cc3ddb0fd5c6317ba2922c3a9177cba
Алексей <pavlof01@gmail.com>
2019-02-14 21:46:53 +0700

    rebuild background images of header for animation in split of them, and ADD Animations

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -8,6 +8,8 @@ import {
   TouchableOpacity,
   AsyncStorage,
   ScrollView,
+  Animated,
+  Easing,
 } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import getZodiacIcon from '../../utils';
@@ -16,7 +18,7 @@ import YourDayCard from '../../components/horoskopesCard';
 
 const DAYS_HOROSKOPES = ['Yesterday', 'Today', 'Tomorrow', 'Weekly', 'Monthly', 'Yearly'];
 
-const { height } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   safeAreaView: {
@@ -24,11 +26,17 @@ const styles = StyleSheet.create({
   },
   header: {
     height: height / 3,
-    justifyContent: 'center',
   },
   headerBackground: {
     width: '100%',
     height: '100%',
+  },
+  headerBackgroundStars: {
+    position: 'absolute',
+    width: '70%',
+    height: '70%',
+    alignSelf: 'center',
+    top: '10%',
   },
   title: {
     color: '#2b2172',
@@ -36,6 +44,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     fontFamily: 'Poppins-Bold',
     paddingLeft: 20,
+    top: '30%',
   },
   settingsIconContainer: {
     position: 'absolute',
@@ -55,6 +64,7 @@ const styles = StyleSheet.create({
     padding: 10,
     /* take height of sign icon and devide to 2 and minus padding (vertical center)  */
     bottom: -(height / 8) / 2 - 10,
+    transform: [{ scale: 1 }],
   },
   circle: {
     position: 'absolute',
@@ -64,13 +74,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#22163c',
     borderRadius: height / 5,
-    bottom: -(height / 5) / 2,
+    top: -(height / 5) / 10,
   },
   two: {
     width: height / 4,
     height: height / 4,
     borderRadius: height / 4,
-    bottom: -(height / 4) / 2,
+    top: -(height / 4) / 6,
   },
   signUserContainer: {
     marginTop: height / 7,
@@ -126,12 +136,104 @@ export default class Home extends Component {
         carrer: false,
         helth: false,
       },
+      headerHeightBackground: new Animated.Value(0),
+      titlePosition: new Animated.Value(0),
+      starsBackgroundHeaderPosition: new Animated.Value(50),
+      userSignSkaleAnim: new Animated.Value(0.3),
+      statAndSignNameContainer: new Animated.Value(300),
+      flatListOfHoroskopesTop: new Animated.Value(200),
+      flatListOfHoroskopesLeft: new Animated.Value(-200),
+      fadeCards: new Animated.Value(0),
+      topCards: new Animated.Value(50),
     };
   }
 
   componentWillMount = async () => {
     const sign = await AsyncStorage.getItem('sign');
     this.setState({ sign });
+  }
+
+  componentDidMount = () => {
+    this._headerBackgroundAnim();
+    this._titleAnim();
+    this._starsBackgroundHeaderAnim();
+    this._userSignAnim();
+    this._userStatsAndSignNameContainerAnim();
+    this._flatListOfHoroskopesAnim();
+    this._cardsAnim();
+  }
+
+  _headerBackgroundAnim = () => {
+    Animated.timing(this.state.headerHeightBackground, {
+      toValue: height / 3,
+      duration: 500,
+    }).start();
+  }
+
+  _titleAnim = () => {
+    Animated.timing(this.state.titlePosition, {
+      toValue: width / 4,
+      duration: 500,
+    }).start();
+  }
+
+  _starsBackgroundHeaderAnim = () => {
+    Animated.timing(this.state.starsBackgroundHeaderPosition, {
+      toValue: width / 4,
+      duration: 500,
+    }).start();
+  }
+
+  _userSignAnim = () => {
+    Animated.sequence([
+      Animated.timing(this.state.userSignSkaleAnim, {
+        toValue: 1.2,
+        duration: 250,
+      }),
+      Animated.timing(this.state.userSignSkaleAnim, {
+        toValue: 1,
+        duration: 250,
+        easing: Easing.bezier(0, 0.71, 0.67, 0.47),
+      }),
+    ]).start();
+  }
+
+  _userStatsAndSignNameContainerAnim = () => {
+    Animated.timing(this.state.statAndSignNameContainer, {
+      toValue: 0,
+      duration: 500,
+      easing: Easing.bezier(0, 0.71, 0.67, 1),
+    }).start();
+  }
+
+  _flatListOfHoroskopesAnim = () => {
+    Animated.parallel([
+      Animated.timing(this.state.flatListOfHoroskopesTop, {
+        toValue: 0,
+        duration: 400,
+        easing: Easing.bezier(0, 0.71, 0.67, 0.7),
+      }),
+      Animated.timing(this.state.flatListOfHoroskopesLeft, {
+        toValue: 0,
+        duration: 400,
+        easing: Easing.bezier(0, 0.71, 0.67, 0.7),
+      }),
+    ]).start();
+  }
+
+  _cardsAnim = () => {
+    Animated.sequence([
+      Animated.timing(this.state.fadeCards, {
+        toValue: 1,
+        duration: 150,
+        easing: Easing.bezier(0, 0.71, 1, 1),
+      }),
+      Animated.timing(this.state.topCards, {
+        toValue: 0,
+        duration: 250,
+        easing: Easing.bezier(0, 0.71, 1, 1),
+      }),
+    ]).start();
   }
 
   userSign = () => {
@@ -168,20 +270,42 @@ export default class Home extends Component {
   keyExtractor = item => `${item}`
 
   render() {
-    const { sign } = this.state;
+    const {
+      sign,
+      headerHeightBackground,
+      titlePosition,
+      starsBackgroundHeaderPosition,
+      userSignSkaleAnim,
+      statAndSignNameContainer,
+      flatListOfHoroskopesTop,
+      flatListOfHoroskopesLeft,
+      fadeCards,
+      topCards,
+    } = this.state;
     const { love, carrer, helth } = this.state.cardExpand;
+    /* turn on/off to show warnings */
+    console.disableYellowBox = true;
     return (
       <ScrollView style={styles.safeAreaView}>
         <View style={styles.header}>
-          <Image
-            style={styles.headerBackground}
+          <Animated.Image
+            style={[styles.headerBackground, { height: headerHeightBackground }]}
             resizeMode="stretch"
             source={require('../../../assets/img/bg-home-header.png')}
           />
-          <View style={styles.userSignContainer}>{this.userSign()}</View>
-          <View style={styles.circle} />
-          <View style={[styles.circle, styles.two]} />
-          <Text style={styles.title}>My Board</Text>
+          <Animated.Image
+            style={[styles.headerBackgroundStars, { right: starsBackgroundHeaderPosition }]}
+            resizeMode="stretch"
+            source={require('../../../assets/img/stars-in-header.png')}
+          />
+          <Animated.View
+            style={[styles.userSignContainer, { transform: [{ scale: userSignSkaleAnim }] }]}
+          >
+            <View style={styles.circle} />
+            <View style={[styles.circle, styles.two]} />
+            {this.userSign()}
+          </Animated.View>
+          <Animated.Text style={[styles.title, { right: titlePosition }]}>My Board</Animated.Text>
           <TouchableOpacity style={styles.settingsIconContainer}>
             <Image
               style={styles.settingsIcon}
@@ -189,22 +313,24 @@ export default class Home extends Component {
             />
           </TouchableOpacity>
         </View>
-        <View style={styles.signUserContainer}>
+        <Animated.View style={[styles.signUserContainer, { top: statAndSignNameContainer }]}>
           <Text style={styles.userSignName}>{sign}</Text>
           <View style={styles.signStatistic}>
             <SignStat rate={2} color="#f5c970" text="Love" />
             <SignStat rate={3} color="#52e092" text="Health" />
             <SignStat rate={0} color="#ff637e" text="Career" />
           </View>
-        </View>
-        <FlatList
-          style={styles.flatList}
-          keyExtractor={this.keyExtractor}
-          data={DAYS_HOROSKOPES}
-          renderItem={this.renderItem}
-          horizontal
-        />
-        <View style={styles.cards}>
+        </Animated.View>
+        <Animated.View style={{ top: flatListOfHoroskopesTop, left: flatListOfHoroskopesLeft }}>
+          <FlatList
+            style={styles.flatList}
+            keyExtractor={this.keyExtractor}
+            data={DAYS_HOROSKOPES}
+            renderItem={this.renderItem}
+            horizontal
+          />
+        </Animated.View>
+        <Animated.View style={[styles.cards, { opacity: fadeCards, top: topCards }]}>
           <YourDayCard
             title="Your Day"
             isToday
@@ -239,7 +365,7 @@ export default class Home extends Component {
             backgroundColorForSetOpacity="rgba(207, 190, 240, 0.8)"
             onExpand={() => this.expandCard('helth')}
           />
-        </View>
+        </Animated.View>
       </ScrollView>
     );
   }
