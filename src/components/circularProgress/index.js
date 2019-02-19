d8d1b09a15f1390c3ca63e44fb9a2ee371e33940
Алексей <pavlof01@gmail.com>
2019-02-19 20:58:57 +0700

    created anim for circullarProgress comeponent

--- src/components/circularProgress/index.js
+++ src/components/circularProgress/index.js
@@ -1,114 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, ViewPropTypes } from 'react-native';
-import { Svg, Path, G } from 'react-native-svg';
+import {
+  Animated,
+  AppState,
+  Easing,
+  View,
+  ViewPropTypes
+} from 'react-native';
+import CircularProgress from './circularProgress';
+const AnimatedProgress = Animated.createAnimatedComponent(CircularProgress);
 
-export default class CircularProgress extends React.PureComponent {
-  polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
-    return {
-      x: centerX + (radius * Math.cos(angleInRadians)),
-      y: centerY + (radius * Math.sin(angleInRadians)),
-    };
+export default class AnimatedCircularProgress extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fillAnimation: new Animated.Value(props.prefill)
+    }
   }
 
-  circlePath(x, y, radius, startAngle, endAngle) {
-    const start = this.polarToCartesian(x, y, radius, endAngle * 0.9999);
-    const end = this.polarToCartesian(x, y, radius, startAngle);
-    const largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
-    const d = [
-      'M', start.x, start.y,
-      'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y,
-    ];
-    return d.join(' ');
+  componentDidMount() {
+    this.animate();
   }
 
-  clampFill = fill => Math.min(100, Math.max(0, fill));
+  componentDidUpdate(prevProps) {
+    if (prevProps.fill !== this.props.fill) {
+      this.animate();
+    }
+  }
+
+  reAnimate(prefill, toVal, dur, ease) {
+    this.setState({
+      fillAnimation: new Animated.Value(prefill)
+    }, () => this.animate(toVal, dur, ease));
+  }
+
+  animate(toVal, dur, ease) {
+    const toValue = toVal || this.props.fill;
+    const duration = dur || this.props.duration;
+    const easing = ease || this.props.easing;
+
+    const anim = Animated.timing(this.state.fillAnimation, {
+      toValue,
+      easing,
+      duration,
+    });
+    anim.start(this.props.onAnimationComplete);
+
+    return anim;
+  }
 
   render() {
-    const {
-      size,
-      width,
-      backgroundWidth,
-      tintColor,
-      backgroundColor,
-      style,
-      rotation,
-      lineCap,
-      arcSweepAngle,
-      fill,
-      children,
-    } = this.props;
-
-    const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, arcSweepAngle);
-    const circlePath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, arcSweepAngle * this.clampFill(fill) / 100);
-    const offset = size - (width * 2);
-
-    const childContainerStyle = {
-      position: 'absolute',
-      left: width,
-      top: width,
-      width: offset,
-      height: offset,
-      borderRadius: offset / 2,
-      alignItems: 'center',
-      justifyContent: 'center',
-      overflow: 'hidden',
-    };
+    const { fill, prefill, ...other } = this.props;
 
     return (
-      <View style={style}>
-        <Svg
-          width={size}
-          height={size}
-          style={{ backgroundColor: 'transparent' }}
-        >
-          <G rotation={rotation} originX={size / 2} originY={size / 2}>
-            { backgroundColor && (
-              <Path
-                d={backgroundPath}
-                stroke={backgroundColor}
-                strokeWidth={backgroundWidth || width}
-                strokeLinecap={lineCap}
-                fill="transparent"
-              />
-            )}
-            <Path
-              d={circlePath}
-              stroke={tintColor}
-              strokeWidth={width}
-              strokeLinecap={lineCap}
-              fill="transparent"
-            />
-          </G>
-        </Svg>
-        {children && (
-          <View style={childContainerStyle}>
-            {children(fill)}
-          </View>
-        )}
-      </View>
+      <AnimatedProgress
+        {...other}
+        fill={this.state.fillAnimation}
+      />
     );
   }
 }
 
-CircularProgress.propTypes = {
-  style: ViewPropTypes.style,
-  size: PropTypes.number.isRequired,
-  fill: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
-  backgroundWidth: PropTypes.number,
-  tintColor: PropTypes.string,
-  backgroundColor: PropTypes.string,
-  rotation: PropTypes.number,
-  lineCap: PropTypes.string,
-  arcSweepAngle: PropTypes.number,
-  children: PropTypes.func,
+AnimatedCircularProgress.propTypes = {
+  ...CircularProgress.propTypes,
+  prefill: PropTypes.number,
+  duration: PropTypes.number,
+  easing: PropTypes.func,
+  onAnimationComplete: PropTypes.func,
 };
 
-CircularProgress.defaultProps = {
-  tintColor: 'black',
-  rotation: 90,
-  lineCap: 'butt',
-  arcSweepAngle: 360,
+AnimatedCircularProgress.defaultProps = {
+  duration: 1000,
+  easing: Easing.out(Easing.ease),
+  prefill: 0,
 };
