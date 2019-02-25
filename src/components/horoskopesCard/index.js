2ef9438193f09c4463fa4ae9a26050672c981def
Алексей <pavlof01@gmail.com>
2019-02-25 19:03:11 +0700

    fixed-card-at-home-screen

--- src/components/horoskopesCard/index.js
+++ src/components/horoskopesCard/index.js
@@ -3,29 +3,50 @@ import {
   Text, StyleSheet, View, Dimensions, Image, TouchableOpacity,
 } from 'react-native';
 import PropTypes from 'prop-types';
-import { setHeightSize, fontSize } from '../../utils';
+import { setHeightSize, setWidthSize, fontSize } from '../../utils';
 
 const { height, width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     width: width / 1.1,
-    height: setHeightSize(60, 60, 60, 60, 50),
     marginHorizontal: 20,
+    marginBottom: 50,
     alignItems: 'center',
     justifyContent: 'center',
+    overflow: 'hidden',
   },
-  imageBackground: {
-    width: '100%',
-    height: '100%',
+  circle: {
     position: 'absolute',
+    top: setHeightSize(-12),
+    right: setHeightSize(-12),
+    width: setHeightSize(30, 30, 30, 28, 26),
+    height: setHeightSize(30, 30, 30, 28, 26),
+    zIndex: 100,
+    borderRadius: setHeightSize(18, 18, 18, 19, 18),
+    overflow: 'hidden',
+  },
+  image: {
+    position: 'absolute',
+    height: setHeightSize(10, 20, 20, 10, 10),
+    width: setWidthSize(20, 30, 30, 30, 30),
+    top: setHeightSize(3, 1, 1, 2, 2),
+    right: setWidthSize(11, 5, 5, 7, 7),
+    zIndex: 150,
+  },
+  todayImage: {
+    top: 2,
+    right: 0,
+    height: setHeightSize(20, 20, 20, 20, 20),
+    width: setWidthSize(50, 60, 60, 55, 60),
   },
   text: {
     color: '#fff',
   },
   bodyCard: {
     padding: setHeightSize(5, 5, 5, 5, 4),
-    position: 'relative',
+    borderRadius: 25,
+    overflow: 'hidden',
   },
   date: {
     color: '#bfdaf9',
@@ -42,21 +63,16 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontFamily: 'Poppins-Light',
     fontSize: fontSize(2, 2, 2, 1.7, 1.8),
+    marginTop: 15,
   },
   blackText: {
     color: '#0d0644',
   },
-  notExpand: {
-    height: setHeightSize(60, 60, 60, 60, 50),
-  },
-  expand: {
-    height: setHeightSize(60, 60, 60, 60, 50),
-  },
   readMoreContainer: {
-    marginLeft: height / 15,
+    marginLeft: setWidthSize(7.2, 9, 9, 9, 8.5),
     position: 'absolute',
-    bottom: -10,
-    height: height / 15,
+    bottom: 10,
+    height: setHeightSize(7.5, 6),
     width: '100%',
     justifyContent: 'flex-end',
   },
@@ -74,17 +90,20 @@ export default class Day extends Component {
       title,
       isToday,
       backgroundImage,
+      backgroundColor,
+      circleColor,
       isExpand,
       readMoreBtnColor,
       backgroundColorForSetOpacity,
       onExpand,
     } = this.props;
     return (
-      <View style={[styles.container, isExpand || isToday ? styles.expand : styles.notExpand]}>
-        <Image style={styles.imageBackground} resizeMode="contain" source={backgroundImage} />
+      <View style={[styles.container]}>
         <View
-          style={[styles.bodyCard, isExpand || isToday ? null : { paddingBottom: height / 50 }]}
+          style={[styles.bodyCard, { backgroundColor }]}
         >
+          {isToday || <View style={[styles.circle, { backgroundColor: circleColor }]} />}
+          <Image resizeMode="contain" style={[styles.image, isToday ? styles.todayImage : null]} source={backgroundImage} />
           <Text style={[styles.text, styles.date]}>{isToday ? '8/09/2019' : null}</Text>
           <Text style={[styles.text, styles.title, !isToday ? styles.blackText : null]}>
             {title}
@@ -126,5 +145,7 @@ Day.propTypes = {
   isExpand: PropTypes.bool,
   readMoreBtnColor: PropTypes.string,
   backgroundColorForSetOpacity: PropTypes.string,
+  backgroundColor: PropTypes.string.isRequired,
+  circleColor: PropTypes.string.isRequired,
   onExpand: PropTypes.func,
 };
